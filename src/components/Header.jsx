5cf9c2fbe2764f062feba370f62af9d6613ad967
zephyr <linjie.zhang@outlook.com>
2023-02-16 16:06:40 +0800

    homepage-version0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,27 +3,45 @@ import { Bars3Icon } from "@heroicons/react/24/outline";
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center p-6 bg-discord_blurple text-black">
+    <div className="flex justify-between items-center p-6 bg-discord_blurple text-white lg:px-10">
       <a href="/">
         <img
-          src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0b5061df290f5892d944_full_logo_black_RGB.svg"
+          src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0b5493894cf60b300587_full_logo_white_RGB.svg"
           alt=""
           className="h-8"
         />
       </a>
       <div className="space-x-4 hidden lg:flex">
-        <a href="" className="nav_link">Download</a>
-        <a href="" className="nav_link">Nitro</a>
-        <a href="" className="nav_link">Discover</a>
-        <a href="" className="nav_link">Safety</a>
-        <a href="" className="nav_link">Support</a>
-        <a href="" className="nav_link">Blog</a>
-        <a href="" className="nav_link">Careers</a>
+        <a href="" className="nav_link">
+          Download
+        </a>
+        <a href="" className="nav_link">
+          Nitro
+        </a>
+        <a href="" className="nav_link">
+          Discover
+        </a>
+        <a href="" className="nav_link">
+          Safety
+        </a>
+        <a href="" className="nav_link">
+          Support
+        </a>
+        <a href="" className="nav_link">
+          Blog
+        </a>
+        <a href="" className="nav_link">
+          Careers
+        </a>
       </div>
       <div className="flex items-center space-x-2 justify-center">
-        <button className="p-2 bg-black text-white text-xs rounded-full px-4 md:text-sm hover:text-discord_blurple hover:shadow-2xl
-        transition duration-200 ease-in-out font-medium">Login</button>
-        <Bars3Icon className="w-9 cursor-pointer lg:hidden"/>
+        <button
+          className="p-2 bg-white text-black text-xs rounded-full px-4 md:text-sm hover:text-discord_blurple hover:shadow-2xl
+        transition duration-200 ease-in-out font-medium"
+        >
+          Login
+        </button>
+        <Bars3Icon className="w-9 cursor-pointer lg:hidden" />
       </div>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { ArrowDownTrayIcon } from "@heroicons/react/24/outline";
+import React from "react";
+import left from "../assets/home-left.svg";
+import right from "../assets/home-right.svg";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="bg-discord_blurple text-white overflow-x-hidden">
+      <div className="h-screen md:h-[85vh] md:flex px-6 py-14 bg-discord_blurple relative">
+        <div className="flex flex-col  lg:justify-center lg:items-center gap-6  lg:max-w-none md:max-w-md">
+          <h1 className="font-extrabold lg:text-[44px] text-2xl">
+            IMAGINE A PLACE...
+          </h1>
+          <h2 className="tracking-wide lg:max-w-3xl w-full">
+            ...where you can belong to a school club, a gaming group, or a
+            worldwide art community. Where just you and a handful of friends can
+            spend time together. A place that makes it easy to talk every day
+            and hang out more often.
+          </h2>
+          <div className="flex flex-col gap-4 sm:flex-row md:flex-col lg:flex-row sm:justify-start md:justify-start">
+            <button className="w-72 bg-white text-black p-4 font-medium rounded-full flex justify-center items-center hover:text-discord_blurple">
+              <ArrowDownTrayIcon className="w-7 mr-2" />
+              Download for Windows
+            </button>
+            <button className="w-72 bg-gray-900 p-4 rounded-full hover:bg-gray-700 font-medium flex items-center justify-center">
+              Open Discord in your browser
+            </button>
+          </div>
+        </div>
+        <div className="flex-grow">
+          <img
+            src={left}
+            alt=""
+            className="absolute -left-36 sm:-lef-44 mt-2 md:hidden"
+          />
+          <img src={right} alt="" className="hidden md:inline absolute" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
